b6ae9076658e309ee8b7710be36b855463e18108
Welliton Inácio <159810533+Wdinac@users.noreply.github.com>
2026-06-09 16:29:20 -0300

    Change expected result of soma test to 99

--- src/test.php
+++ src/test.php
@@ -12,9 +12,10 @@ assert_options(ASSERT_CALLBACK, function($file, $line, $code, $desc = null) {
 require_once __DIR__ . '/Calculator.php';
 
 try {
+    throw new Exception('Falha proposital para testar o GitHub Actions');
     // -------- TESTES DE SOMA --------
     assert(
-        Calculator::soma(2, 3) === 5,
+        Calculator::soma(2, 3) === 99,
         'soma(2, 3) deve retornar 99'
     );
 
