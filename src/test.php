7bfffefc716f0e6bb0c41d17a46aefb557d8a17f
Welliton Inácio <159810533+Wdinac@users.noreply.github.com>
2026-06-09 16:23:56 -0300

    Update test.php

--- src/test.php
+++ src/test.php
@@ -14,8 +14,8 @@ require_once __DIR__ . '/Calculator.php';
 try {
     // -------- TESTES DE SOMA --------
     assert(
-        Calculator::soma(2, 3) === 123,
-        'Falha Proposital'
+        Calculator::soma(2, 3) === 5,
+        'soma(2, 3) deve retornar 99'
     );
 
     assert(
